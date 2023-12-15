52cd7365c18e3ec9f86e93178a9369659183c132
naga <77886419+naga-karupi@users.noreply.github.com>
2023-12-15 10:34:36 +0900

    Update download.sh

--- download.sh
+++ download.sh
@@ -5,9 +5,9 @@ rm -rf ydlidar
 mkdir ydlidar; cd ydlidar
 git clone https://github.com/YDLIDAR/ydlidar_ros2_driver.git
 
-sudo apt install cmake pkg-config
-sudo apt install swig
-sudo apt install python3-pip
+sudo apt install cmake pkg-config -y
+sudo apt install swig -y
+sudo apt install python3-pip -y
 git clone https://github.com/YDLIDAR/YDLidar-SDK.git
 mkdir YDLidar-SDK/build
 cd YDLidar-SDK/build
